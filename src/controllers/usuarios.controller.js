04a2b95d4ba4d6878d6115015d225d63a7aa6a78
Orlando Galvan Vargas <159957306+OrlandoGalvanVargas@users.noreply.github.com>
2024-04-02 21:56:44 -0600

    Boton de agregar perliculas v1

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,4 +1,6 @@
 import { pool } from "../db.js";
+import multer from 'multer'; 
+import fs from 'fs'; 
 
 // Modificar get
 export const getUsuarios = async (req, res) => {
@@ -49,7 +51,7 @@ export const getDescripcionUsuario = async (req, res) => {
 };
 
 
-export const postUsuarios = async (req, res) => {
+export const postUsuarios = async (req, res) => {         
   try {
     const { strNombreUsuario, strContraseña, idTipoUsuario, idTipoEstado } = req.body;
 
@@ -130,5 +132,90 @@ export const deleteUsuarios = async (req, res) => {
     return res.status(500).json({
       message: "Algo salio mal",
     });
+  }
+};
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); 
+  }
+});
+
+const upload = multer({ storage: storage });
+
+export const postPelicula = async (req, res) => {
+  try {
+    const { strNombre, strSinopsis, intDuracion, idEstadoPelicula, generos } = req.body;
+    
+    const imageData = fs.readFileSync(req.file.path);
+    const imageBuffer = Buffer.from(imageData);
+
+    const [result1] = await pool.query('SELECT strNombre FROM peli_peliculas where strNombre = ?', [strNombre]);
+    if(result1.length == 0){
+      const [result] = await pool.query('INSERT INTO peli_peliculas (strNombre, strSinopsis, intDuracion, bloImagen, idEstadoPelicula) VALUES (?, ?, ?, ?, ?)', [strNombre, strSinopsis, intDuracion, imageBuffer, idEstadoPelicula]);
+
+      const idPelicula = result.insertId;
+  
+      for (const idGenero of generos) {
+        await pool.query('INSERT INTO peli_generos (idPelicula, idGeneroPelicula) VALUES (?, ?)', [idPelicula, idGenero]);
+      }
+      res.status(200).json({ message: 'Película insertada exitosamente' });
+    }else{
+      return res.status(500).json({ message: 'Error al insertar la película' });
+    }
+
+    
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getGenero = async (req, res) => {
+  const { genero } = req.query;
+
+  try {
+    // Consulta SQL para obtener el ID del género basado en el nombre
+    const [result] = await pool.query('SELECT id FROM peli_cat_genero WHERE strGenero = ?', [genero]);
+
+    if (result.length === 0) {
+      // Si no se encuentra ningún género con ese nombre, devuelve un código de estado 404
+      return res.status(404).json({ message: 'Género no encontrado' });
+    }
+
+    const { id } = result[0];
+    res.json({ id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al buscar el género' });
+  }
+};
+
+export const getPelicula = async (req, res) => {
+  try {
+      const { id } = req.params;
+      
+      // Consulta para obtener la imagen de la película por su ID
+      const [result] = await pool.query('SELECT bloImagen FROM peli_peliculas WHERE id = ?', [id]);
+
+      // Si no se encuentra la película con el ID especificado
+      if (result.length === 0) {
+          return res.status(404).send('No se encontró ninguna película con el ID especificado');
+      }
+
+      // Selecciona la imagen de la fila obtenida
+      const { bloImagen } = result[0];
+      
+      // Envía la imagen al cliente
+      res.set('Content-Type', 'image'); // Ajusta el tipo de contenido según sea necesario
+      res.send(bloImagen);
+
+  } catch (error) {
+      console.error('Error al obtener la imagen de la película:', error);
+      res.status(500).send('Error interno del servidor');
   }
 };
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,6 +1,17 @@
 import { Router } from "express";
-import {getUsuarios,getUsuario, postUsuarios, updateUsuarios,deleteUsuarios, getDescripcionUsuario } from "../controllers/usuarios.controller.js";
+import {getUsuarios,getUsuario, postUsuarios, updateUsuarios,deleteUsuarios, getDescripcionUsuario, postPelicula, getPelicula, getGenero } from "../controllers/usuarios.controller.js";
+import multer from 'multer';
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); 
+  }
+});
+
+const upload = multer({ storage: storage });
 const router = Router()
 
 //
@@ -15,5 +26,11 @@ router.post('/usuario',postUsuarios)
 router.put('/usuario/:id', updateUsuarios)
 
 router.delete('/usuario/:id', deleteUsuarios)
+
+router.post('/pelicula', upload.single('imagen'), postPelicula);
+
+router.get('/pelicula/:id', getPelicula);
+
+router.get('/genero', getGenero);
 
 export default router;
